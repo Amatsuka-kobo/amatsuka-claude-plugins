78853347babaaa8c0c58178db1d22d40a372bcdf
phyllis98 <phyllisphillis998@gmail.com>
2026-07-26 12:14:06 +0900

    chore: 環境変数設定処理の最適化

--- scripts/install-proxy.sh
+++ scripts/install-proxy.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 # CLIProxyAPI のインストールスクリプト
 
+# bashrc などに環境変数を設定する関数
+set_env() {
+  COMMENT="# Claude Code (CLIProxyAPI)"
+  EXPORT_ANTHROPIC_AUTH_TOKEN="export ANTHROPIC_AUTH_TOKEN="
+  EXPORT_CLAUDE_CODE_ENABLE_GATEWAY_MODEL_DISCOVERY="export CLAUDE_CODE_ENABLE_GATEWAY_MODEL_DISCOVERY=1"
+
+  if ! grep -q "$EXPORT_ANTHROPIC_AUTH_TOKEN" "$2"; then
+    if grep -q "$COMMENT" "$2"; then
+      sed -i "/$COMMENT/a $EXPORT_ANTHROPIC_AUTH_TOKEN$1" "$2"
+    else
+      echo "
+$COMMENT
+$EXPORT_ANTHROPIC_AUTH_TOKEN$1
+" >> "$2"
+    fi
+  fi
+
+  if ! grep -q "$EXPORT_CLAUDE_CODE_ENABLE_GATEWAY_MODEL_DISCOVERY" "$2"; then
+    sed -i "/$EXPORT_ANTHROPIC_AUTH_TOKEN/a $EXPORT_CLAUDE_CODE_ENABLE_GATEWAY_MODEL_DISCOVERY" "$2"
+  fi
+}
+
 # インストール済みか確認
 if [ -e "$HOME/.local/bin/cli-proxy-api" ]; then
   echo "CLIProxyAPI is already installed."
@@ -19,25 +41,23 @@ else
 fi
 
 # config ファイルの作成
+API_KEY=""
 if [ ! -e ./cliproxyapi.config.yaml ]; then
   cp ./cliproxyapi.config.example.yaml ./cliproxyapi.config.yaml
 
   # API キーの作成と永続化 export
   API_KEY="sk-$(openssl rand -base64 24)"
   sed -i "s|replace-with-a-random-local-key|${API_KEY}|g" ./cliproxyapi.config.yaml
+else
+  # 既存 API キー取得
+  API_KEY=$(grep "sk-" cliproxyapi.config.yaml | sed 's/^.*-[[:space:]]*"//; s/"[[:space:]]*$//')
+fi
 
-  EXPORT="
-# Claude Code (CLIProxyAPI)
-export ANTHROPIC_AUTH_TOKEN=${API_KEY}
-"
-  if [ -e "$HOME/.profile" ]; then
-    echo "$EXPORT" >> "$HOME/.profile"
-    source "$HOME/.profile"
-  elif [ -e "$HOME/.bash_profile"]; then
-    echo "$EXPORT" >> "$HOME/.bash_profile"
-    source "$HOME/.bash_profile"
-  else
-    echo "$EXPORT" >> "$HOME/.bashrc"
-    source "$HOME/.bashrc"
-  fi
+# 環境変数の設定
+if [ -e "$HOME/.profile" ]; then
+  set_env "$API_KEY" "$HOME/.profile"
+elif [ -e "$HOME/.bash_profile"]; then
+  set_env "$API_KEY" "$HOME/.bash_profile"
+else
+  set_env "$API_KEY" "$HOME/.bashrc"
 fi
